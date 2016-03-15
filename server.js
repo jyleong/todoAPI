3dd8de63f9588e057e879c37ce69bb020b8e3977
James Leong <jamesleong@Jamess-MacBook-Pro.local>
2016-03-14 19:58:49 -0700

    searching with new query param

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/',function(req,res) {
 	res.send('TODO api root');
 });
 
-// GET /todos?completed=true
+// GET /todos?completed=true&q=house
 app.get('/todos', function(req,res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -20,6 +20,12 @@ app.get('/todos', function(req,res) {
 		filteredTodos = _.where(todos, {completed: true});
 	} else if(queryParams.hasOwnProperty('completed') && queryParams.completed ==='false') {
 		filteredTodos = _.where(todos, {completed: false});
+	}
+	
+	if(queryParams.hasOwnProperty('q') &&queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function(todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
 	}
 	res.json(filteredTodos);
 });
